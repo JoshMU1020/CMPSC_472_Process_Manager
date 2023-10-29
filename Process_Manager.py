2f7b171c97810f279948dce364f3f5d2b6389fa4
JoshMU1020 <118846876+JoshMU1020@users.noreply.github.com>
2023-10-28 23:47:55 -0400

    Update Process_Manager.py

--- Process_Manager.py
+++ Process_Manager.py
@@ -1,3 +1,4 @@
+import os  # python .\print.py.txt
 import threading
 import multiprocessing
 import time
@@ -11,7 +12,7 @@ class ProcessManager:
     def __init__(self):
         self.process_pids = []
         self.threads = []
-        #self.lock = threading.Lock()  # Synchronization lock
+
         self.queue = multiprocessing.Queue()
         self.thread_message_queue = multiprocessing.Queue()
 
@@ -24,38 +25,39 @@ class ProcessManager:
         self.total_items = 0
 
     def worker_function(self):
-        #print(f"Worker process PID: {os.getpid()}")
-        time.sleep(5)  # Simulate some work
+        logging.info(f"Worker process PID: {os.getpid()}")
+        work_time = random.randint(1, 5) * 10
+        time.sleep(work_time)  # Simulate some work
         return
 
-    def thread_worker_function(self):
-        # with self.lock:  # Acquire the lock
+    """def thread_worker_function(self):
         logging.info(f"Worker thread PID: {threading.current_thread().ident}")
-        # print(f"Worker thread PID: {threading.current_thread().ident}")
         time.sleep(5)
-        return
+        return"""
 
-    def create_and_start_process(self):
+    def create_and_start_process(self, uses_t):
         logging.info("Creating new process...")
         self.total_items = random.randint(2, 5) * 4  # Randomly generate number of data items
-        new_process_instance = multiprocessing.Process(target=self.synchronize_threads)
+        if uses_t:
+            new_process_instance = multiprocessing.Process(target=self.synchronize_threads)
+        else:
+            new_process_instance = multiprocessing.Process(target=self.worker_function)
         process_pid = new_process_instance.pid
+
         new_process_instance.start()
         self.process_pids.append(new_process_instance)
         logging.info(f"Process created with PID: {process_pid}")
 
-        # Add data to the IPC queue
-        self.queue.put(f"Data from Process {process_pid}")
+        self.queue.put(f"Data from Process {process_pid}")  # Add data to the IPC queue
 
-        #self.synchronize_threads()
-
-        #sleep_time = random.randint(2, 5) * 4  # Random multiple of 5 between 5 and 60
-        #num_threads = int(sleep_time / 10)
-        #self.create_and_start_thread(num_threads)
+        """self.synchronize_threads()
+        sleep_time = random.randint(2, 5) * 4  # Random multiple of 5 between 5 and 60
+        num_threads = int(sleep_time / 10)
+        self.create_and_start_thread(num_threads)"""
 
         return new_process_instance
 
-    def create_and_start_thread(self, num_threads):
+    '''def create_and_start_thread(self, num_threads):
         threads = []
         for _ in range(num_threads):
             thread = threading.Thread(target=self.thread_worker_function)
@@ -64,7 +66,7 @@ class ProcessManager:
 
         for thread in threads:
             thread.join()
-        return
+        return'''
 
     def get_process_info_by_pid(self, target_pidi):
         try:
@@ -80,19 +82,24 @@ class ProcessManager:
             return f"Process {target_pidi} does not exist."
 
     def process_management_list(self):
+        logging.info(f"Process list of ran/running processes.")
         process_info_list = []
         for p_id in self.process_pids:
-            process = psutil.Process(p_id.pid)
-            process_info = {
-                "PID": process.pid,
-                "Name": process.name(),
-                "Status": process.status(),
-                "Parent PID": process.ppid(),
-            }
-            process_info_list.append(process_info)
+            try:
+                process = psutil.Process(p_id.pid)
+                process_info = {
+                    "PID": process.pid,
+                    "Status": process.status(),
+                    "Parent PID": process.ppid(),
+                }
+                process_info_list.append(process_info)
+            except psutil.NoSuchProcess:
+                # Handle the case where the process is not found
+                logging.error(f"Process with PID {p_id.pid} not found.")
         return process_info_list
 
     def terminate_process(self, targeted_pid):
+        logging.info(f"Attempting to delete process with PID: {targeted_pid}")
         for process in self.process_pids:
             if process.pid == targeted_pid:
                 if process.is_alive():
@@ -108,6 +115,18 @@ class ProcessManager:
                 logging.info(f"Process {targeted_pid} not found in the list of processes.")
                 return f"Process {targeted_pid} not found in the list of processes."
 
+    def terminate_all(self):
+        logging.info("Attempting to terminate all processes")
+        for process in self.process_pids:
+            if process.is_alive():
+                process.terminate()
+                process.join()
+                self.process_pids.remove(process)
+                logging.info(f"Process {process.pid} has been forcibly terminated.")
+            else:
+                logging.info(f"Process {process.pid} is already completed.")
+        return
+
     def synchronize_threads(self):
         logging.info("Synchronizing Threads")
         producers = [
@@ -122,26 +141,40 @@ class ProcessManager:
         for consumer_thread in consumers:
             consumer_thread.start()
 
+        time.sleep(5)
+
+        for producer_thread in producers:
+            producer_thread.join()
+        for consumer_thread in consumers:
+            consumer_thread.join()
+
     def producer(self, buffer, mutex, empty, data, min_value, max_value, pro_id):
+        logging.info("Creating producer...")
         for i in range(self.total_items):
             item = random.randint(min_value, max_value)
+            logging.info("Aquiring resources...")
             empty.acquire()
             mutex.acquire()
             buffer.append(item)
             logging.info(f"Producer {pro_id} Produced {item}. Buffer: {buffer}")
+            logging.info("Releasing resources...")
             mutex.release()
             data.release()
             time.sleep(random.uniform(0.1, 0.5))
 
     def consumer(self, buffer, mutex, empty, data, con_id):
+        logging.info("Creating consumer thread...")
         for i in range(self.total_items // 2):
+            logging.info("Aquiring resources...")
             data.acquire()
             mutex.acquire()
             if buffer:
                 item = buffer.pop(0)
                 logging.info(f"Consumer {con_id} Consumed {item}. Buffer: {buffer}")
+            logging.info("Releasing resources...")
             mutex.release()
             empty.release()
+            time.sleep(random.uniform(0.1, 0.5))
 
     def send_message(self, message):
         """Simulate one thread sending a message to another."""
@@ -157,6 +190,7 @@ class ProcessManager:
             logging.info(f"Thread {threading.current_thread().ident} received no messages.")
 
     def ipc_operations(self):
+        logging.info("Displaying IPC Operations Menu...")
         print("\nIPC Operations Menu:")
         print("1. Message Passing (Between Threads)")
         ipc_choice = input("Enter your choice: ")
@@ -172,6 +206,14 @@ class ProcessManager:
             thread1.join()
             thread2.join()
 
+    def display_log_contents(self, log_file):
+        try:
+            with open(log_file, 'r') as file:
+                log_contents = file.read()
+                print(log_contents)
+        except FileNotFoundError:
+            print(f"Log file '{log_file}' not found.")
+
 
 if __name__ == '__main__':
     # Your existing code for the menu
@@ -179,9 +221,13 @@ if __name__ == '__main__':
                         datefmt='%Y-%m-%d %H:%M:%S')
     with open('process_manager.log', 'w'):
         pass
+
     manager = ProcessManager()
 
+    new_process = manager.create_and_start_process(False)
+
     while True:
+        time.sleep(1)
         logging.info("Displaying Process Manager Menu...")
         print("\nProcess Manager Menu:")
         print("1. Create Process")
@@ -192,11 +238,8 @@ if __name__ == '__main__':
 
         choice = input("Enter your choice: ")
 
-        #new_process = manager.create_and_start_process()
-        #time.sleep(1)
-
         if choice == '1':
-            new_process = manager.create_and_start_process()
+            new_process = manager.create_and_start_process(False)
             time.sleep(1)
             target_pid = new_process.pid
             result = manager.get_process_info_by_pid(target_pid)
@@ -242,15 +285,15 @@ if __name__ == '__main__':
             print("2. IPC Operations")
             thread_choice = input("Enter your choice: ")
             if thread_choice == '1':
-                manager.synchronize_threads()  # Call the synchronization method
+                manager.create_and_start_process(True)  # Call the synchronization method
             elif thread_choice == '2':
                 manager.ipc_operations()  # Call the IPC operations method
 
         elif choice == '4':
-            # Implement logger menu here
-            pass
+            manager.display_log_contents('process_manager.log')
 
         elif choice == '5':
+            manager.terminate_all()
             exit()
 
         else:
